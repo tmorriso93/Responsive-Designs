3340e8d70dec6467e77fec73fce26303e8d6a0bf
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2021-06-28 12:52:01 -0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Javascript for ttthe toggle
-
-let mainNav = document.getElementById("menu");
-let navToggle = document.getElementById("js-navbar");
-navToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-});
